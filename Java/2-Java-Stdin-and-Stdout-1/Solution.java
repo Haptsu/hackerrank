b1d7994c453d54d46a33a20d5e0194c004ab8744
Haptsu <Haptsu@users.noreply.github.com>
2015-11-11 11:11:24 -0600

    Create Solution.java

---
+++ Java/2-Java-Stdin-and-Stdout-1/Solution.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+      Scanner sc=new Scanner(System.in);
+      int a=sc.nextInt();
+      //Complete this line
+      //Complete this line
+         
+      System.out.println(a);
+      //Complete this line
+      //Complete this line
+      
+    
+    }
+}
