d9d4512766d6c3c9640c43102a381b083d8ab2a6
Haptsu <Haptsu@users.noreply.github.com>
2015-11-11 11:34:17 -0600

    Create Template.java

---
+++ Java/3-Java-If-Else/Template.java
@@ -0,0 +1,23 @@
+    import java.io.*;
+    import java.util.*;
+    import java.text.*;
+    import java.math.*;
+    import java.util.regex.*;
+
+    public class Solution {
+
+        public static void main(String[] args) {
+
+            Scanner sc=new Scanner(System.in);
+            int n=sc.nextInt();            
+            String ans;
+            if(n%2==1)ans = "Weird";
+            else
+            {
+               //Complete the code
+                
+            }
+            System.out.println(ans);
+            
+        }
+    }
