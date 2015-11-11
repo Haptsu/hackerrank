1dcc62ab4cb6fc81f96664ec6b2e2f0225e395f3
Haptsu <Haptsu@users.noreply.github.com>
2015-11-11 11:21:41 -0600

    Update Solution.java
    Add Stdin and Stdout for two more ints (b and c)

--- Java/2-Java-Stdin-and-Stdout-1/Solution.java
+++ Java/2-Java-Stdin-and-Stdout-1/Solution.java
@@ -5,12 +5,12 @@ public class Solution {
     public static void main(String[] args) {
       Scanner sc=new Scanner(System.in);
       int a=sc.nextInt();
-      //Complete this line
-      //Complete this line
+      int b=sc.nextInt();
+      int c=sc.nextInt();
          
       System.out.println(a);
-      //Complete this line
-      //Complete this line
+      System.out.println(b);
+      System.out.println(c);
       
     
     }
